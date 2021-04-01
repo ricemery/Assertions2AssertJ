f67bb79cbd65786736bdaa9a94ccf0d0c563e781
Ric Emery <ric.emery@gmail.com>
2021-04-01 08:50:25 -0700

    Add ability to convert assertions in nested classes

--- src/main/kotlin/com/chainstaysoftware/testing/FileHandler.kt
+++ src/main/kotlin/com/chainstaysoftware/testing/FileHandler.kt
@@ -1,5 +1,6 @@
 package com.chainstaysoftware.testing
 
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -16,26 +17,14 @@ class FileHandler {
    fun handle(psiFile: PsiFile) {
       var codeModified = false
       val imports = mutableSetOf<Pair<String, String>>()
+      val project = psiFile.project
 
       psiFile.children
          .filterIsInstance<PsiClass>()
          .forEach {
-
-            it.allMethods.forEach { psiMethod ->
-               psiMethod.accept(object : PsiRecursiveElementVisitor() {
-                  override fun visitElement(psiElement: PsiElement) {
-                     val handler = handlers.firstOrNull { handler -> handler.canHandle(psiElement) }
-                     when {
-                        handler != null -> {
-                           val imps = handler.handle(psiFile.project, psiElement)
-                           imports.addAll(imps)
-                           codeModified = true
-                        }
-                        else -> super.visitElement(psiElement)
-                     }
-                  }
-               })
-            }
+            val result = handlePsiClass(it, project)
+            codeModified = codeModified || result.first
+            imports.addAll(result.second)
          }
 
       if (codeModified) {
@@ -50,5 +39,43 @@ class FileHandler {
             Util.addStaticImport(psiFile.project, psiFile, import.first, import.second)
          }
       }
+   }
+
+   /**
+    * Handle conversion of asserts within a class. Recurses to handle inner classes.
+    * Returns a Pair where the left value indicates if any statements where modified
+    * and the right value is a set of imports that will need to be added to the class.
+    */
+   private fun handlePsiClass(
+      psiClass: PsiClass,
+      project: Project,
+   ): Pair<Boolean, Set<Pair<String, String>>> {
+      var codeModified = false
+      val imports = mutableSetOf<Pair<String, String>>()
+
+      psiClass.allInnerClasses.forEach {
+         val result = handlePsiClass(it, project)
+         codeModified = codeModified || result.first
+         imports.addAll(result.second)
+      }
+
+      psiClass.allMethods.forEach { psiMethod ->
+         psiMethod.accept(object : PsiRecursiveElementVisitor() {
+            override fun visitElement(psiElement: PsiElement) {
+               val handler =
+                  handlers.firstOrNull { handler -> handler.canHandle(psiElement) }
+               when {
+                  handler != null -> {
+                     val imps = handler.handle(project, psiElement)
+                     imports.addAll(imps)
+                     codeModified = true
+                  }
+                  else -> super.visitElement(psiElement)
+               }
+            }
+         })
+      }
+
+      return Pair(codeModified, imports)
    }
 }
